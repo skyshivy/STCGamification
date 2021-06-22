c2cb92a3c15a5bd7b07f37165a731165cebfe5eb
Shiv <sky.shivy@gmail.com>
2021-06-22 14:04:17 +0530

    framework added

--- STCGamification/ViewController.swift
+++ STCGamification/ViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import STCGamification
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Game.openGame(controller: self, mobileNumber: "9902390347", language: "EN", nationalId: "PCMR390347", nationalIdType: "1", gameType: "GuessNWin", apiKey: "DJA5J4NOQESW82gFEExRnK0uVzzJ0RV1wsPyEEi", gameid: nil, token: "J0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9", complition: complitionHandler(action:))
         // Do any additional setup after loading the view.
     }
+//    func complitionHandler(action: GameAction) {
+//        switch action {
+//        case .dismiss:
+//            print("controller dismissed")
+//        case .onSuccess:
+//            print("on success")
+//        case .tokenExpired:
+//            print("toke expired call back")
+//            DispatchQueue.main.asyncAfter(deadline: .now()+4) {
+//                    Game.updateToken(token: "J0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9")
+//            }
+//        default:
+//            print("default")
+//        }
+//    }
 
 
 }
