18863fc33ffb03fb69563ab3b5526461bd59edbd
Shiv <sky.shivy@gmail.com>
2021-06-29 14:43:33 +0530

    new framework added

---
+++ STCGamification.xcframework/ios-arm64_armv7/STCGamification.framework/Headers/STCGamification.h
@@ -0,0 +1,18 @@
+//
+//  STCGamification.h
+//  STCGamification
+//
+//  Created by SKY on 22/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STCGamification.
+FOUNDATION_EXPORT double STCGamificationVersionNumber;
+
+//! Project version string for STCGamification.
+FOUNDATION_EXPORT const unsigned char STCGamificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STCGamification/PublicHeader.h>
+
+
